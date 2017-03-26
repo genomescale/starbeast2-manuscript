25ab742723fc720ac7cf6a804cba36b997f4d190
Huw A. Ogilvie <huw.ogilvie@anu.edu.au>
2017-03-26 23:55:14 +1100

    Finishing script descriptions

--- scripts/lognormal_mcmc/plot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-Rscript plot_gene_branch_rates.R
-Rscript plot_gene_node_heights.R
-Rscript plot_gene_topology_frequencies.R
-Rscript plot_species_branch_rates.R
-Rscript plot_species_node_heights.R
-Rscript plot_species_topology_frequencies.R
-
