2c16ed9c0bd512c172497e1647ff02b31200fd54
Huw A. Ogilvie <huw.ogilvie@anu.edu.au>
2017-03-31 12:54:55 +1100

    Put scripts in right place

--- scripts/21-species/topology_accuracy_phased.R
+++ scripts/21-species/topology_accuracy_phased.R
@@ -17,19 +17,19 @@ levels(topology_accuracy$clock) = c("strict clock", "GT-UCLN", "ST-UCLN")
 topology_accuracy$analysis = factor(paste(topology_accuracy$method, topology_accuracy$n_loci),
 levels = paste(rep(levels(topology_accuracy$method), c(3, 3, 3)), levels(topology_accuracy$n_loci)))
 
-dodge = position_dodge(width = 0.9)
+dodge = position_dodge(width = 0.5)
 
 mcc_rf_plot = ggplot(topology_accuracy, aes(x = analysis, y = mcc_rf_mean, color = clock)) +
-  geom_point(stat = "identity", position = dodge) +
-  geom_errorbar(aes(ymin = mcc_rf_low, ymax = mcc_rf_high), width = 0.5, position = dodge) +
+  geom_point(stat = "identity", position = dodge, size = 2) +
+  geom_errorbar(aes(ymin = mcc_rf_low, ymax = mcc_rf_high), width = 0.4, position = dodge) +
   scale_y_continuous(breaks = seq(0, 10, 1)) +
   ylab("Mean MCC RF error") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1.0), axis.title.x = element_blank(), legend.position = "none", legend.title = element_blank()) +
   background_grid(major = "y", minor = "none")
 
 in_cs_plot = ggplot(topology_accuracy, aes(x = analysis, y = in_cs_prop, color = clock)) +
-  geom_point(stat = "identity", position = dodge) +
-  geom_errorbar(aes(ymin = in_cs_low, ymax = in_cs_high), width = 0.5, position = dodge) +
+  geom_point(stat = "identity", position = dodge, size = 2) +
+  geom_errorbar(aes(ymin = in_cs_low, ymax = in_cs_high), width = 0.4, position = dodge) +
   ylab("True topology in credible set") +
   scale_y_continuous(labels = scales::percent) +
   theme(axis.text.x = element_blank(), axis.title.x = element_blank(), axis.ticks.x = element_blank(), legend.position = "top", legend.title = element_blank()) +
